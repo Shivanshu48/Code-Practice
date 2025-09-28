a30cb0df761f026a1a2132b659ebf4784baaf4a6
Shivanshu Pandey <165388592+Shivanshu48@users.noreply.github.com>
2025-09-28 23:41:08 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (28.20%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,24 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n == 0) return 1.0;
+        if(x == 0) return 0.0;
+        if(x == 1) return 1.0;
+        
+        long binf = n;
+
+        if(n < 0){
+            x = 1 / x;
+            binf = -binf;
+        }
+        double ans = 1;
+        while(binf > 0){
+            if(binf % 2 == 1){
+                ans = ans * x;
+
+            }
+            x = x * x;
+            binf = binf / 2;
+        }
+        return ans;
+    }
+}
