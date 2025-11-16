35b3afa9bb8224a97ce37860be85dcae0ad1e372
Shivanshu Pandey <165388592+Shivanshu48@users.noreply.github.com>
2025-11-16 12:25:33 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (38.25%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.cpp
@@ -13,25 +13,22 @@ public:
     ListNode* reverseBetween(ListNode* head, int left, int right) {
         if(!head || left == right) return head;
 
-        // Step 1: Create dummy
         ListNode* dummy = new ListNode(0);
         dummy->next = head;
 
-        // Step 2: Move prev to node before 'left'
         ListNode* prev = dummy;
         for(int i=1; i<left; i++){
             prev = prev->next;
         }
 
-        // Step 3: Reverse sublist
-        ListNode* start = prev->next;
-        ListNode* second = start->next;
+        ListNode* curr = prev->next;
+        ListNode* second = curr->next;
 
         for(int i=0; i<right-left; i++){
-            start->next = second->next;
+            curr->next = second->next;
             second->next = prev->next;
             prev->next = second;
-            second = start->next;
+            second = curr->next;
         }
 
         return dummy->next;
