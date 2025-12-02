5e8e18965c6d10d04e6e93d862134873097991fd
Shivanshu Pandey <165388592+Shivanshu48@users.noreply.github.com>
2025-12-03 00:55:09 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (15.59%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int N;
-    bool checkBiparDFS(vector<vector<int>>& adj, int curr, vector<int>& color, int currColor){
+    bool checkBiparBFS(vector<vector<int>>& adj, int curr, vector<int>& color, int currColor){
+        queue<int> q;
+        q.push(curr);
         color[curr] = currColor;
-        for(int &v : adj[curr]){
-            if(color[v] == color[curr]){
-                return false;
-            }
-            if(color[v] == -1){
-                int colorV = 1-currColor;
-                if(checkBiparDFS(adj, v, color, colorV) == false){
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            for(int &v : adj[u]){
+                if(color[v] == color[u]){
                     return false;
+                }
+                else if(color[v] == -1){
+                    color[v] = 1-color[u];
+                    q.push(v);
                 }
             }
         }
@@ -22,7 +26,7 @@ public:
         vector<int> color(N, -1);
         for(int i=0; i<N; i++){
             if(color[i] == -1){
-                if(checkBiparDFS(graph, i, color, 1) == false){
+                if(checkBiparBFS(graph, i, color, 1) == false){
                     return false;
                 }
             }
