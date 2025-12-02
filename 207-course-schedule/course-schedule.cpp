826235e389200d913da081be471e12e5a7588c3e
Shivanshu Pandey <165388592+Shivanshu48@users.noreply.github.com>
2025-12-02 11:34:35 +0530

    Time: 7 ms (48.66%) | Memory: 19.9 MB (44.14%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool topologicalSortCheck(unordered_map<int, vector<int>>& adj, int n, vector<int>& indegree){
+        int count = 0;
+        queue<int> q;
+        for(int i=0; i<n; i++){
+            if(indegree[i] == 0){
+                count++;
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()){
+            int u = q.front();
+            q.pop();
+            for(int& v : adj[u]){
+                indegree[v]--;
+                if(indegree[v] == 0){
+                    count++;
+                    q.push(v);
+                }            
+            }
+        }
+        if(count == n) return true;
+        return false;
+    }
+    
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, vector<int>> adj;
+        vector<int> indegree(numCourses,0);
+        for(auto& vec : prerequisites){
+            int a = vec[0];
+            int b = vec[1];
+
+            adj[b].push_back(a);
+            indegree[a]++;
+            
+        }
+        return topologicalSortCheck(adj, numCourses, indegree);
+    }
+};
