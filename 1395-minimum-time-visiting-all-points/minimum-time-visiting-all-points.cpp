42bfce87a0fe64ed6e8f5849e07ff2fdd89d9f04
Shivanshu Pandey <165388592+Shivanshu48@users.noreply.github.com>
2026-01-12 21:54:54 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (26.76%) - LeetSync

---
+++ 1395-minimum-time-visiting-all-points/minimum-time-visiting-all-points.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int totaltime = 0;
+        for(int i = 1; i < points.size(); i++){
+            int distonx = abs(points[i][0] - points[i-1][0]);
+            int distony = abs(points[i][1] - points[i-1][1]);
+            
+            totaltime += max(distonx, distony);
+        }
+        return totaltime;
+    }
+};
