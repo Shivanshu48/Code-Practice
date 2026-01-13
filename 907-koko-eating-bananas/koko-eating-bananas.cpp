58c417c46fa4b63db90dbdaff8ee7a84fe4db994
Shivanshu Pandey <165388592+Shivanshu48@users.noreply.github.com>
2026-01-13 19:42:00 +0530

    Time: 12 ms (28.32%) | Memory: 22.9 MB (76.16%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int totalcapacity(vector<int>& piles, int mid, int h){
+        int  sumhours = 0;
+        for(auto& i : piles){
+            sumhours += i/mid;
+            if(i%mid != 0){
+                sumhours++;
+            }
+        }
+        return sumhours <= h;
+    }
+
+    
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int n = piles.size();
+        int l = 1;
+        int r = *max_element(begin(piles), end(piles));
+
+        while(l < r){
+            int mid = l + (r-l)/2;
+            if(totalcapacity(piles, mid, h)){
+                r = mid;
+            }
+            else{
+                l = mid+1;
+            }
+        }
+        return l;
+    }
+};
