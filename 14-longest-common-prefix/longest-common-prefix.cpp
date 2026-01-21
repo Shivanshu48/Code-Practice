6de229f72626f1b82fbaf98fc46a9574e6183e66
Shivanshu Pandey <165388592+Shivanshu48@users.noreply.github.com>
2026-01-22 01:24:07 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (16.76%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int n = strs.size();
+        string prefix = strs[0];
+
+        for(int i = 0; i < n; i++){
+            while(strs[i].find(prefix) != 0){
+                prefix = prefix.substr(0, prefix.length()-1);
+            }
+        }
+        return prefix;
+    }
+};
