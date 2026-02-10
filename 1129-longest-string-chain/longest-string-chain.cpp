9fcd6e2dda8d698aa57af9b3d128a76160c8299e
Shivanshu Pandey <165388592+Shivanshu48@users.noreply.github.com>
2026-02-10 14:04:46 +0530

    Time: 55 ms (48.27%) | Memory: 22.7 MB (42.54%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -22,26 +22,19 @@ public:
         return word1.length() < word2.length();
     }
 
-    int lis(vector<string>& words, int p, int i){
-        if(i == n) return 0;
-        if(p != -1 && t[p][i] != -1) return t[p][i];
-
-        int take = 0, skip = 0;
-
-        if(p == -1 || isPre(words[p], words[i])){
-            take = 1 + lis(words, i, i+1);
-        }
-        skip = lis(words, p, i+1);
-
-        if(p != -1){
-            t[p][i] = max(take, skip);
-        }
-        return max(take,skip);
-    }
     int longestStrChain(vector<string>& words) {
-        memset(t, -1, sizeof(t));
         n = words.size();
         sort(words.begin(), words.end(), check);
-        return lis(words, -1, 0);
+        vector<int> t(n, 1);
+        int maxl = 1;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                if(isPre(words[j], words[i])){
+                    t[i] = max(t[i], t[j]+1);
+                    maxl = max(maxl, t[i]);
+                }
+            }
+        }
+        return maxl;
     }
 };
