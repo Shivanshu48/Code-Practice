250c9a52ad804a3efc42c9885696fd50c1a72929
Shivanshu Pandey <165388592+Shivanshu48@users.noreply.github.com>
2026-02-17 11:24:42 +0530

    Time: 32 ms (80.39%) | Memory: 53.4 MB (83.74%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,36 +1,44 @@
 class Solution {
 public:
-    vector<vector<string>> ans;
-    vector<string> temp;
-
-    bool isPalindrome(string &s, int l, int r){
-        while(l < r){
-            if(s[l] != s[r])
-                return false;
-            l++;
-            r--;
-        }
-        return true;
-    }
-
-    void solve(int idx, string &s){
-        if(idx == s.size()){
-            ans.push_back(temp);
-            return;
+    
+    void solve(string &s, int i, vector<string>& currpath, vector<vector<bool>>& t, vector<vector<string>>& res){
+        if(i == s.length()){
+            res.push_back(currpath);
+            return; 
         }
 
-        for(int i=idx; i<s.size(); i++){
-            if(isPalindrome(s, idx, i)){
-                temp.push_back(s.substr(idx, i-idx+1));
-                solve(i+1, s);
-                temp.pop_back();
+        for(int j = i; j < s.length(); j++){
+            if(t[i][j] == true){
+                currpath.push_back(s.substr(i, j-i+1));
+                solve(s, j+1, currpath, t, res);
+                currpath.pop_back();
             }
         }
     }
 
-    
-    vector<vector<string>> partition(string s){
-        solve(0, s);
-        return ans;
+    vector<vector<string>> partition(string s) {
+        int n = s.length();
+        vector<vector<bool>> t(n, vector<bool>(n, false));
+        for(int i = 0; i < n; i++){
+            t[i][i] = true;
+        }
+        for(int L = 2; L <= n; L++){
+            for(int i = 0; i < n-L+1; i++){
+                int j = i+L-1;
+                if(s[i] == s[j]){
+                    if(L == 2){
+                        t[i][j] = true;
+                    }
+                    else{
+                        t[i][j] = t[i+1][j-1];
+                    }
+                }
+            }
+        }
+
+        vector<vector<string>> res;
+        vector<string> currpath;
+        solve(s, 0, currpath, t, res);
+        return res;
     }
 };
