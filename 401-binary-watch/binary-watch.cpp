d9014f27c7221b8f9c089acda9cd0f3a09909bd7
Shivanshu Pandey <165388592+Shivanshu48@users.noreply.github.com>
2026-02-17 08:55:14 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (9.64%) - LeetSync

--- 401-binary-watch/binary-watch.cpp
+++ 401-binary-watch/binary-watch.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
-    vector<string> readBinaryWatch(int turnedOn) {
-        vector<string> result;
-        for (int h = 0; h < 12; h++) {
-            for (int m = 0; m < 60; m++) {
-                if (__builtin_popcount(h) + __builtin_popcount(m) == turnedOn) {
-                    string time = to_string(h) + ":";
-                    if (m < 10) time += "0";
-                    time += to_string(m);
-                    result.push_back(time);
-                }
-            }
+    vector<string> result;
+    vector<int> leds = {8,4,2,1,32,16,8,4,2,1};
+
+    void dfs(int idx, int turnedOn, int hour, int minute) {
+        if (hour > 11 || minute > 59) return;
+
+        if (turnedOn == 0) {
+            string time = to_string(hour) + ":";
+            if (minute < 10) time += "0";
+            time += to_string(minute);
+            result.push_back(time);
+            return;
         }
+
+        for (int i = idx; i < 10; i++) {
+            if (i < 4)
+                dfs(i + 1, turnedOn - 1, hour + leds[i], minute);
+            else
+                dfs(i + 1, turnedOn - 1, hour, minute + leds[i]);
+        }
+    }
+    
+    vector<string> readBinaryWatch(int turnedOn) {
+        dfs(0, turnedOn, 0, 0);
         return result;
     }
 };
