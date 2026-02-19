2a4d44a80cee56265d0a5ae7c8419453b47e58d6
Shivanshu Pandey <165388592+Shivanshu48@users.noreply.github.com>
2026-02-19 22:36:26 +0530

    Time: 0 ms (100.00%) | Memory: 27.6 MB (95.15%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -3,29 +3,33 @@ public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-
-        int max_sq[m][n];
-        int max_side_length = 0;
-
-        for(int j=0; j<n; j++) {                
-            max_sq[0][j] = matrix[0][j] - '0';
-            if(matrix[0][j] == '1')
-                max_side_length = 1;
-        }
-        for(int i=0; i<m; i++) {
-            max_sq[i][0] = matrix[i][0] - '0';
-            if(matrix[i][0] == '1')
-                max_side_length = 1;
-        }
-        for(int i=1; i<m; i++) {
-            for(int j=1; j<n; j++) {
-                if(matrix[i][j] == '0')
-                    max_sq[i][j] = 0;
-                else
-                    max_sq[i][j] = 1 + min(min(max_sq[i-1][j], max_sq[i-1][j-1]), max_sq[i][j-1]);
-                max_side_length = max(max_side_length, max_sq[i][j]);                
+        int dp[m][n];
+        int maxs = 0;
+        for(int row = 0; row < m; row++){
+            dp[row][0] = matrix[row][0] - '0';
+            if(matrix[row][0] == '1'){
+                maxs = 1;
             }
         }
-        return max_side_length * max_side_length;
+
+        for(int col = 0; col < n; col++){
+            dp[0][col] = matrix[0][col] - '0';
+            if(matrix[0][col] == '1'){
+                maxs = 1;
+            }
+        }
+        
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                if(matrix[i][j] == '0'){
+                    dp[i][j] = 0;
+                }
+                else{
+                    dp[i][j] = 1 + min(min(dp[i-1][j], dp[i-1][j-1]), dp[i][j-1]);
+                }
+                maxs = max(maxs, dp[i][j]);                
+            }
+        }
+        return maxs * maxs;
     }
 };
