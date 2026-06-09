2d7d9fd0cf69d16d36fc73049da3a75158722e10
Shivanshu Pandey <165388592+Shivanshu48@users.noreply.github.com>
2026-06-09 14:53:16 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (91.95%) - LeetSync

---
+++ 365-water-and-jug-problem/water-and-jug-problem.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int gcd(int a, int b){
+        while(b != 0){
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+    bool canMeasureWater(int x, int y, int target) {
+        if(target > x+y){
+            return false;
+        }
+        return target % gcd(x, y) == 0;
+    }
+};
