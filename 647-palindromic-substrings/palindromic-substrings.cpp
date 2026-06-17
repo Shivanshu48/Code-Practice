f89168aed8dc9a0380128a0cf79231096b646de9
Shivanshu Pandey <165388592+Shivanshu48@users.noreply.github.com>
2026-06-17 20:53:18 +0530

    Time: 1857 ms (5.00%) | Memory: 419 MB (7.24%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.cpp
+++ 647-palindromic-substrings/palindromic-substrings.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
+    
+    int t[1001][1001];
+    bool find(string s, int i, int j){
+        if(i > j){
+            return true;
+        }
+
+        if(t[i][j] != -1){
+            return t[i][j];
+        }
+
+        if(s[i] == s[j]){
+            return t[i][j] = find(s, i+1, j-1);
+        }
+        return t[i][j] = false;
+    }
     int countSubstrings(string s) {
         int n = s.length();
-        vector<vector<bool>> t(n, vector<bool> (n, false));
-        int count = 0;
-        for(int L = 1; L <= n; L++){
-            for(int i = 0; L+i-1 < n; i++){
-                int j = i+L-1;
-                if(i == j){
-                    t[i][j] = true;
-                }
-                else if(i+1 == j){
-                    t[i][j] = (s[i] == s[j]);
-                }
-                else{
-                    t[i][j] = ((s[i] == s[j]) && t[i+1][j-1]);
-                }
-
-                if(t[i][j] == true){
-                    count++;
+        memset(t, -1, sizeof(t));
+        int c = 0;
+        for(int i = 0; i < n; i++){
+            for(int j = i; j < n; j++){
+                if(find(s, i, j)){
+                    c++;
                 }
             }
         }
-        return count;
+        return c;
     }
 };
